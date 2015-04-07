cba50675600069fd52205f187a5d2b3d20f94dc3
Andreas Demetriou <androsland@gmail.com>
2015-04-08 00:54:54 +0300

    imporved to support NinePatch images

--- MLRoundedImageView.java
+++ MLRoundedImageView.java
@@ -1,15 +1,59 @@
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.NinePatchDrawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
 public class MLRoundedImageView extends ImageView {
+
+	Drawable drawable;
+	Bitmap bitmap;
+	Canvas c;
 
 	public MLRoundedImageView(Context context) {
 		super(context);
+		drawable = getDrawable();
+		if (drawable instanceof BitmapDrawable) {
+
+		} else {
+			bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+					drawable.getIntrinsicHeight(), Config.ARGB_8888);
+			c = new Canvas(bitmap);
+		}
 	}
 
 	public MLRoundedImageView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		drawable = getDrawable();
+		if (drawable instanceof BitmapDrawable) {
+
+		} else {
+			bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+					drawable.getIntrinsicHeight(), Config.ARGB_8888);
+			c = new Canvas(bitmap);
+		}
 	}
 
 	public MLRoundedImageView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+		drawable = getDrawable();
+		if (drawable instanceof BitmapDrawable) {
+
+		} else {
+			bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+					drawable.getIntrinsicHeight(), Config.ARGB_8888);
+			c = new Canvas(bitmap);
+		}
 	}
 
 	@Override
@@ -24,7 +68,16 @@ public class MLRoundedImageView extends ImageView {
 		if (getWidth() == 0 || getHeight() == 0) {
 			return;
 		}
-		Bitmap b = ((BitmapDrawable) drawable).getBitmap();
+
+		Bitmap b;
+		if (drawable instanceof BitmapDrawable) {
+			b = ((BitmapDrawable) drawable).getBitmap();
+		} else {
+			drawable.setBounds(0, 0, c.getWidth(), c.getHeight());
+			drawable.draw(c);
+			b = bitmap;
+		}
+
 		Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
 
 		int w = getWidth(), h = getHeight();
@@ -36,17 +89,18 @@ public class MLRoundedImageView extends ImageView {
 
 	public static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
 		Bitmap sbmp;
-		
+
 		if (bmp.getWidth() != radius || bmp.getHeight() != radius) {
 			float smallest = Math.min(bmp.getWidth(), bmp.getHeight());
 			float factor = smallest / radius;
-			sbmp = Bitmap.createScaledBitmap(bmp, (int)(bmp.getWidth() / factor), (int)(bmp.getHeight() / factor), false);
+			sbmp = Bitmap.createScaledBitmap(bmp,
+					(int) (bmp.getWidth() / factor),
+					(int) (bmp.getHeight() / factor), false);
 		} else {
 			sbmp = bmp;
 		}
-		
-		Bitmap output = Bitmap.createBitmap(radius, radius,
-				Config.ARGB_8888);
+
+		Bitmap output = Bitmap.createBitmap(radius, radius, Config.ARGB_8888);
 		Canvas canvas = new Canvas(output);
 
 		final int color = 0xffa19774;
@@ -58,12 +112,11 @@ public class MLRoundedImageView extends ImageView {
 		paint.setDither(true);
 		canvas.drawARGB(0, 0, 0, 0);
 		paint.setColor(Color.parseColor("#BAB399"));
-		canvas.drawCircle(radius / 2 + 0.7f,
-				radius / 2 + 0.7f, radius / 2 + 0.1f, paint);
+		canvas.drawCircle(radius / 2 + 0.7f, radius / 2 + 0.7f,
+				radius / 2 + 0.1f, paint);
 		paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
 		canvas.drawBitmap(sbmp, rect, rect, paint);
 
 		return output;
 	}
-
 }
